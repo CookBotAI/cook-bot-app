5056725dd5ba9cad81bde936c59746e27207d14d
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-05 18:41:38 -0700

    added test branch

--- src/assets/components/Recipe.jsx
+++ src/assets/components/Recipe.jsx
@@ -1,89 +1,98 @@
-import React from "react";
-import axios from "axios";
-import AddModal from "./AddModal";
+import React from 'react';
+import axios from 'axios';
+import AddModal from './AddModal';
 import { Button } from 'react-bootstrap';
 // import { withAuth0 } from "@auth0/auth0-react";
 
 const PORT = import.meta.env.VITE_SERVER_URL;
 
 class Recipe extends React.Component {
+  state = {
+    recipes: [],
+    showModal: false,
+  };
 
-    state = {
-        recipes: [],
-        showModal: false,
-    }
+  componentDidMount() {
+    this.fetchRecipes();
+  }
 
-    componentDidMount() {
-        this.fetchRecipes();
-    }
+  handleShowModal = () => {
+    this.setState({ showModal: true });
+  };
 
-    handleShowModal = () => {
-        this.setState({ showModal: true });
-    }
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
 
-    handleCloseModal = () => {
-        this.setState({ showModal: false });
-    }
+  //GET//
+  fetchRecipes = async () => {
+    axios.get(`${PORT}/recipes`).then((response) => {
+      this.setState({ recipes: response.data });
+      console.log(response.data);
+    });
+  };
 
-    //GET//
-    fetchRecipes = async () => {
-        axios.get(`${PORT}/recipes`)
-        .then(response => {
-            this.setState({recipes: response.data})
-            console.log(response.data)
-        });
-    }
+  //POST//
+  addRecipe = (ingredients) => {
+    // console.log('Im here before post request', ingredients);
+    // axios.post(`${PORT}/recipes`, {ingredients})
+    // .then(response => {
+    //     this.setState({recipes: [...this.state.recipes, response.data]})
+    //     console.log(response.data)
+    // });
+    //  handleCreate = (book) => {
+    const config = {
+      method: 'POST',
+      baseURL: `${PORT}`,
+      url: '/books',
+      data: {ingredients},
+    };
+     console.log('Im here before post request', {ingredients});
+     axios(config).then(response => {
+        this.setState({recipes: [...this.state.recipes, response.data]})
+        console.log(response.data)
+    });
+  };
 
-    //POST//
-    addRecipe = async (ingredients) => {
-        console.log('Im here before post request');
-        axios.post(`${PORT}/recipes`, ingredients)
-        .then(response => {
-            this.setState({recipes: [...this.state.recipes, response.data]})
-            console.log(response.data)
-        });
-    }
+  //PUT//
+  updateRecipe = async (id, updatedData) => {
+    axios.put(`${PORT}/recipes/${id}`, updatedData).then((response) => {
+      const updatedRecipes = this.state.recipes.map((recipe) => {
+        if (recipe.id === id) {
+          return response.data;
+        }
+        return recipe;
+      });
+      this.setState({ recipes: updatedRecipes });
+      console.log(response.data);
+    });
+  };
 
-    //PUT//
-    updateRecipe = async (id, updatedData) => {
-        axios.put(`${PORT}/recipes/${id}`, updatedData)
-        .then(response => {
-            const updatedRecipes = this.state.recipes.map(recipe => {
-                if (recipe.id === id) {
-                    return response.data;
-                }
-                return recipe;
-            });
-            this.setState({recipes: updatedRecipes});
-            console.log(response.data)
-        });
-    }
+  //DELETE//
+  deleteRecipe = async (id) => {
+    axios.delete(`${PORT}/recipes/${id}`).then((response) => {
+      const filteredRecipes = this.state.recipes.filter(
+        (recipe) => recipe.id !== id
+      );
+      this.setState({ recipes: filteredRecipes });
+      console.log(response.data);
+    });
+  };
 
-    //DELETE//
-    deleteRecipe = async (id) => {
-        axios.delete(`${PORT}/recipes/${id}`)
-        .then(response => {
-            const filteredRecipes = this.state.recipes.filter(recipe => recipe.id !== id);
-            this.setState({recipes: filteredRecipes});
-            console.log(response.data)
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <Button variant="outline-success" onClick={this.handleShowModal}>Add New Recipe</Button>
-                <AddModal 
-                    show={this.state.showModal} 
-                    onHide={this.handleCloseModal}
-                    addRecipe={this.addRecipe}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Button variant="outline-success" onClick={this.handleShowModal}>
+          Add New Recipe
+        </Button>
+        <AddModal
+          show={this.state.showModal}
+          onHide={this.handleCloseModal}
+          addRecipe={this.addRecipe}
+        />
+      </div>
+    );
+  }
 }
 
 export default Recipe;
-
-
-
